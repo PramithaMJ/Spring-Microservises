eead37cca13883124409088ec5a3427fcaf58374
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-11-26 21:06:20 +0530

    Add Question Wrapper

--- quizapp/quizapp/src/main/java/com/pmj/quizapp/controller/QuizController.java
+++ quizapp/quizapp/src/main/java/com/pmj/quizapp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.pmj.quizapp.controller;
 
+import com.pmj.quizapp.model.Question;
+import com.pmj.quizapp.model.QuestionWrapper;
 import com.pmj.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -18,6 +19,10 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam int numQ,@RequestParam String title){
         return quizService.createQuiz(category,numQ,title);
+    }
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuestion(@PathVariable Integer id){
+        return quizService.getQuizQuestion(id);
     }
 
 }
--- quizapp/quizapp/src/main/java/com/pmj/quizapp/service/QuizService.java
+++ quizapp/quizapp/src/main/java/com/pmj/quizapp/service/QuizService.java
@@ -3,13 +3,16 @@ package com.pmj.quizapp.service;
 import com.pmj.quizapp.dao.QuestionDao;
 import com.pmj.quizapp.dao.QuizDao;
 import com.pmj.quizapp.model.Question;
+import com.pmj.quizapp.model.QuestionWrapper;
 import com.pmj.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -32,5 +35,18 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(Integer id) {
+
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+        for (Question q: questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getQuestionId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+            questionForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionForUser,HttpStatus.OK);
     }
 }
